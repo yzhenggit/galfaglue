c5e90f80bdecb0a42001a110dee770baa0ef49b0
Yong Zheng <yz2505@columbia.edu>
2014-12-09 13:44:46 -0500

    add 'layer' to the viewer

--- glue_galfahi_rgb_viewer.py
+++ glue_galfahi_rgb_viewer.py
@@ -5,40 +5,42 @@ import numpy as np
 import pyfits
 
 # define the galfa-hi viewer
-galfa_rgb_viewer = custom_viewer('GALFA-RGB Viewer',  # name of viewer
-                   center_kms = (-600, 600),          # center of velocity in km/s
-                   width_kms=(1, 50),                 # width of RGB channel in km/s
-                   data='att',                        # dropdown to choose data attribute to image
+galfa_rgb_viewer = custom_viewer('GALFA-HI RGB Viewer',  # name of viewer
+                   data = 'att',
+                   center = (-600, 600),          # center of velocity in km/s
+                   width =(1, 50),                 # width of RGB channel in km/s
                    redraw_on_settings_change = True   # rerender data if settings change
                   )
 
 
-@galfa_viewer.plot_data
-def draw(axes, data, ra, dec, vlsr, center_kms, width_kms):
+@galfa_rgb_viewer.plot_data
+def draw(axes, data, center, width, layer):
     """
-    Make a 3 color image showing a cube slice (g), slice - width (r), slice + width (b)
+    Make a 3 color image showing a data slice (g), slice - width (r), slice + width (b)
     data: 3D numpy arrays
     ra, dec, vlsr: 1D array, showing the grid points on each of the axis
     """
     if data is None or data.size == 0 or data.ndim != 3:
         return
+ 
+    velocity = layer['Velocity', :, 0, 0]
+    dec = layer['Declination', 0, :, 0]
+    ra = layer['Right Ascension', 0, 0, :]
 
-    delta_vlsr = vlsr[1] - vlsr[0]
-    width_pixel = int(width_kms/delta_vlsr)
+    velo_pix = velocity[1] - velocity[0]
 
-    hw_pixel = np.ceil(width_pixel/2.)
+    npix_width = int(width/velo_pix)
+    npix_hw    = np.ceil(npix_width/2.)
 
-    # extract rgb sub_cubes with some velocity width
-    center_pixel = (center_kms-vlsr.min())/delta_vlsr
+    # extract rgb sub_datas with some velocity width
+    cenpix_g  = int((center - velocity.min())/velo_pix)
+    g = data[max(cenpix_g - npix_hw, 0):min(cenpix_g + npix_hw, data.shape[0]-1), :, :]
 
-    cen_g  = center_pixel
-    g = data[max(cen_g - hw_pixel, 0):min(cen_g + hw_pixel, data.shape[0]-1), :, :]
+    cenpix_r  = cenpix_g - npix_hw
+    r = data[max(cenpix_r - npix_hw, 0):min(cenpix_r + npix_hw, data.shape[0]-1), :, :]
 
-    cen_r  = center_pixel -hw_pixel
-    r = data[max(cen_r - hw_pixel, 0):min(cen_r + hw_pixel, data.shape[0]-1), :, :]
-
-    cen_b = center_pixel + hw_pixel
-    b = data[max(cen_b - hw_pixel, 0):min(cen_b + hw_pixel, data.shape[0]-1), :, :]
+    cenpix_b = cenpix_g + npix_hw
+    b = data[max(cenpix_b - npix_hw, 0):min(cenpix_b + npix_hw, data.shape[0]-1), :, :]
 
     # take the mean for each r, g, b slice	
     g = np.mean(g, axis = 0)
@@ -55,7 +57,8 @@ def draw(axes, data, ra, dec, vlsr, center_kms, width_kms):
     norm.stretch = 'arcsinh'
 
     rgb = norm(rgb)
-    axes.imshow(rgb, origin='lower', extent = [ra.max(), ra.min(), dec.min(), dec.max()], aspect = 1.)
+    axes.imshow(rgb, origin='lower', extent = [ra.max(), ra.min(), dec.min(), dec.max()])
     axes.set_xlabel('RA')
     axes.set_ylabel('DEC')
+    # axes.set_title(data.label)
 
